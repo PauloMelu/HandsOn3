21d7528624ea45dc4a99bf896c56a8ff4833cd35
Ian Paulo <ianpaulomelu@gmail.com>
2023-02-17 00:17:22 +0000

    Prob3

---
+++ TickTockOClock.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class TickTockOClock{
+    public static void main(String [] args){
+        
+        for(int i=1;i<=12;i++){
+            System.out.println(i + " o'clock");
+        }
+    }
+}
